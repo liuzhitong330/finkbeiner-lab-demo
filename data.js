4e57e6e65b5f97600e59b7f52415004b29b66f54
Cathy LIU <liuzhitong330@gmail.com>
2026-09-07 19:27:15 -0700

    Add data.js

---
+++ data.js
@@ -0,0 +1,190 @@
+window.FINKBEINER_DEMO_DATA = {
+  "source": "finkbeiner-lab/microscopeimagequality",
+  "commit": "4190ba36d7e672e7b430516b7c3a59c8294efe0b",
+  "defaultZ": 4,
+  "defaultExposure": 50,
+  "zOffsets": [
+    0,
+    1,
+    2,
+    3,
+    4,
+    6,
+    8
+  ],
+  "exposures": [
+    25,
+    50,
+    100
+  ],
+  "conditions": {
+    "z0_e25": {
+      "z": 0,
+      "exposure": 25,
+      "focusRetention": 100.0,
+      "saturation": 0.0,
+      "p99Intensity": 26.1,
+      "image": "assets/z0_e25.png"
+    },
+    "z1_e25": {
+      "z": 1,
+      "exposure": 25,
+      "focusRetention": 90.56,
+      "saturation": 0.0,
+      "p99Intensity": 25.97,
+      "image": "assets/z1_e25.png"
+    },
+    "z2_e25": {
+      "z": 2,
+      "exposure": 25,
+      "focusRetention": 72.81,
+      "saturation": 0.0,
+      "p99Intensity": 25.57,
+      "image": "assets/z2_e25.png"
+    },
+    "z3_e25": {
+      "z": 3,
+      "exposure": 25,
+      "focusRetention": 59.34,
+      "saturation": 0.0,
+      "p99Intensity": 25.33,
+      "image": "assets/z3_e25.png"
+    },
+    "z4_e25": {
+      "z": 4,
+      "exposure": 25,
+      "focusRetention": 50.52,
+      "saturation": 0.0,
+      "p99Intensity": 24.91,
+      "image": "assets/z4_e25.png"
+    },
+    "z6_e25": {
+      "z": 6,
+      "exposure": 25,
+      "focusRetention": 40.24,
+      "saturation": 0.0,
+      "p99Intensity": 24.1,
+      "image": "assets/z6_e25.png"
+    },
+    "z8_e25": {
+      "z": 8,
+      "exposure": 25,
+      "focusRetention": 35.92,
+      "saturation": 0.0,
+      "p99Intensity": 23.52,
+      "image": "assets/z8_e25.png"
+    },
+    "z0_e50": {
+      "z": 0,
+      "exposure": 50,
+      "focusRetention": 100.0,
+      "saturation": 0.0,
+      "p99Intensity": 52.05,
+      "image": "assets/z0_e50.png"
+    },
+    "z1_e50": {
+      "z": 1,
+      "exposure": 50,
+      "focusRetention": 90.56,
+      "saturation": 0.0,
+      "p99Intensity": 51.79,
+      "image": "assets/z1_e50.png"
+    },
+    "z2_e50": {
+      "z": 2,
+      "exposure": 50,
+      "focusRetention": 72.81,
+      "saturation": 0.0,
+      "p99Intensity": 50.99,
+      "image": "assets/z2_e50.png"
+    },
+    "z3_e50": {
+      "z": 3,
+      "exposure": 50,
+      "focusRetention": 59.34,
+      "saturation": 0.0,
+      "p99Intensity": 50.5,
+      "image": "assets/z3_e50.png"
+    },
+    "z4_e50": {
+      "z": 4,
+      "exposure": 50,
+      "focusRetention": 50.52,
+      "saturation": 0.0,
+      "p99Intensity": 49.66,
+      "image": "assets/z4_e50.png"
+    },
+    "z6_e50": {
+      "z": 6,
+      "exposure": 50,
+      "focusRetention": 40.24,
+      "saturation": 0.0,
+      "p99Intensity": 48.04,
+      "image": "assets/z6_e50.png"
+    },
+    "z8_e50": {
+      "z": 8,
+      "exposure": 50,
+      "focusRetention": 35.92,
+      "saturation": 0.0,
+      "p99Intensity": 46.88,
+      "image": "assets/z8_e50.png"
+    },
+    "z0_e100": {
+      "z": 0,
+      "exposure": 100,
+      "focusRetention": 100.0,
+      "saturation": 1.65,
+      "p99Intensity": 100.0,
+      "image": "assets/z0_e100.png"
+    },
+    "z1_e100": {
+      "z": 1,
+      "exposure": 100,
+      "focusRetention": 91.28,
+      "saturation": 1.58,
+      "p99Intensity": 100.0,
+      "image": "assets/z1_e100.png"
+    },
+    "z2_e100": {
+      "z": 2,
+      "exposure": 100,
+      "focusRetention": 74.47,
+      "saturation": 1.53,
+      "p99Intensity": 100.0,
+      "image": "assets/z2_e100.png"
+    },
+    "z3_e100": {
+      "z": 3,
+      "exposure": 100,
+      "focusRetention": 61.16,
+      "saturation": 1.42,
+      "p99Intensity": 100.0,
+      "image": "assets/z3_e100.png"
+    },
+    "z4_e100": {
+      "z": 4,
+      "exposure": 100,
+      "focusRetention": 52.24,
+      "saturation": 1.24,
+      "p99Intensity": 99.17,
+      "image": "assets/z4_e100.png"
+    },
+    "z6_e100": {
+      "z": 6,
+      "exposure": 100,
+      "focusRetention": 41.68,
+      "saturation": 0.27,
+      "p99Intensity": 95.93,
+      "image": "assets/z6_e100.png"
+    },
+    "z8_e100": {
+      "z": 8,
+      "exposure": 100,
+      "focusRetention": 37.2,
+      "saturation": 0.0,
+      "p99Intensity": 93.61,
+      "image": "assets/z8_e100.png"
+    }
+  }
+};
